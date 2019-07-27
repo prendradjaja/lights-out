b414540acf014741e8a95e40e4068888f01ef09b
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2019-07-26 22:40:51 -0700

    mnemonics: one of these was wrong!!

--- validator.py
+++ validator.py
@@ -1,7 +1,11 @@
 def main():
     for line in open('mnemonics', 'r'):
         if is_data_line(line):
-            check_line(line)
+            try:
+                check_line(line)
+            except AssertionError:
+                print(line)
+                raise
 
 def is_data_line(line):
     parts = line.split()
@@ -27,13 +31,30 @@ def check_line(line):
     except ValueError:
         print(parts)
         raise
+    in_bin_raw = in_bin
+    out_bin_raw = out_bin
     in_dec = int(in_dec)
     out_dec = int(remove_comment(out_dec))
     in_bin = parse_binary(in_bin)
     out_bin = parse_binary(out_bin)
     assert in_dec == in_bin
     assert out_dec == out_bin
+    assert get_mask(in_bin_raw) == out_bin_raw
     print(in_dec, 'ok')
+
+def get_mask(in_bin_raw):
+    masks = {
+        0: 0b11000,
+        1: 0b11100,
+        2: 0b01110,
+        3: 0b00111,
+        4: 0b00011
+    }
+    out = 0
+    for (i, char) in enumerate(in_bin_raw):
+        if char == '1':
+            out ^= masks[i]
+    return format(out, '05b').replace('0', '.')
 
 def parse_binary(s):
     return int(s.replace('.', '0'), 2)
